63e403fda805577c5bc685af0452316bf6c5cc45
HyperHunterJ <159351131+HyperHunterJ@users.noreply.github.com>
2025-05-27 15:25:03 -0400

    Updated Theme File Editor Name Again

--- wp-content/themes/bricks-child-2/functions.php
+++ wp-content/themes/bricks-child-2/functions.php
@@ -96,7 +96,7 @@ add_action( 'admin_bar_menu', 'add_custom_admin_bar_link', 999 );
 function add_custom_admin_bar_link( $admin_bar ) {
 	$admin_bar->add_menu( array(
 		'id'    => 'theme-editor',
-		'title' => 'Theme File Edit',
+		'title' => 'Theme File Edits',
 		'href'  => home_url($path = '/wp-admin/theme-editor.php', $scheme = 'https'),
 	) );
 }
