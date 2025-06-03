16ddfe33e67518147c269ae396fc189323c20067
Hunter <hunter@MacBook-Pro-2.local>
2025-06-03 11:07:39 -0400

    Added activation and deactivation hooks

--- wp-content/plugins/HyperSiteReviews/HyperSiteReviews.php
+++ wp-content/plugins/HyperSiteReviews/HyperSiteReviews.php
@@ -6,4 +6,35 @@
  * Requires PHP: 8.4
 */
 
+/**
+ * Register the "Reviews" custom post type
+ */
+function hsrev_setup_post_type() {
+	register_post_type( 'book', ['public' => true ] ); 
+} 
+add_action( 'init', 'hsrev_setup_post_type' );
+
+
+/**
+ * Activate the plugin.
+ */
+function hsrev_activate() { 
+	// Trigger our function that registers the custom post type plugin.
+	hsrev_setup_post_type(); 
+	// Clear the permalinks after the post type has been registered.
+	flush_rewrite_rules(); 
+}
+register_activation_hook( __FILE__, 'hsrev_activate' );
+
+/**
+ * Deactivation hook.
+ */
+function hsrev_deactivate() {
+	// Unregister the post type, so the rules are no longer in memory.
+	unregister_post_type( 'book' );
+	// Clear the permalinks to remove our post type's rules from the database.
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'hsrev_deactivate' );
+
 ?>
