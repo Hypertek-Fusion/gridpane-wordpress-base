a48d6158611bc175b472b903c3278bc8f49a9c23
Hunter <hunter@MacBook-Pro-2.local>
2025-06-03 14:43:14 -0400

    Added debug setting form submission logic

--- wp-content/plugins/HyperSiteReviews/includes/admin/templates/debug-settings-page.php
+++ wp-content/plugins/HyperSiteReviews/includes/admin/templates/debug-settings-page.php
@@ -1,9 +1,11 @@
 <div class="wrap">
     <h1>HyperSite Reviews Debug Settings</h1>
     <form method="post">
-        <?php wp_nonce_field('hsrev_setup'); ?>
+        <?php wp_nonce_field('hsrev_debug_setting_set'); ?>
         <p>Handle any settings for debug purposes here.</p>
+        <label>Setup Complete? : </label>
         <input type="checkbox" name="is-setup" <?php echo get_option('hsrev_setup_complete') ? 'checked' : ''; ?>>
+        <label>Bypass Setup? : </label>
         <input type="checkbox" name="bypass-setup-page">
         <input type="submit" value="Complete Setup" class="button button-primary">
     </form>
