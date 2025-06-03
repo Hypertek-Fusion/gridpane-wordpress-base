9ba058f4d3c4eb499dcd5e59de2b0be108ec7f93
Hunter <hunter@MacBook-Pro-2.local>
2025-06-03 13:57:19 -0400

    Added debug settings

--- wp-content/plugins/HyperSiteReviews/includes/admin/templates/debug-settings-page.php
+++ wp-content/plugins/HyperSiteReviews/includes/admin/templates/debug-settings-page.php
@@ -1,8 +1,10 @@
 <div class="wrap">
-    <h1>HyperSite Reviews Setup</h1>
+    <h1>HyperSite Reviews Debug Settings</h1>
     <form method="post">
         <?php wp_nonce_field('hsrev_setup'); ?>
-        <p>Welcome! Let's get your plugin set up.</p>
+        <p>Handle any settings for debug purposes here.</p>
+        <input type="checkbox" name="is-setup" <?php echo get_option('hsrev_setup_complete') ? 'checked' : ''; ?>>
+        <input type="checkbox" name="bypass-setup-page">
         <input type="submit" value="Complete Setup" class="button button-primary">
     </form>
 </div>
