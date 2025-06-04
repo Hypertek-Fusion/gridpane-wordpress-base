e847afbed2839eed2156be2e5e9ca3aba3fe04f0
Hunter <hunter@MacBook-Pro-2.local>
2025-06-04 15:55:55 -0400

    Added disable button functionality

--- wp-content/plugins/HyperSiteReviews/admin/js/setup-page.js
+++ wp-content/plugins/HyperSiteReviews/admin/js/setup-page.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const accountSelect = document.getElementById('account-selection-table');
     const accountCheckboxes = accountSelect.querySelectorAll('input[type="checkbox"]');
+    const accountSelectButton = document.querySelector('button[data-page="1"]');
 
     accountCheckboxes.forEach(c => {
         c.addEventListener('change', e => {
             const clickedCheckbox = e.target;
-
             const otherCheckboxes = Array.from(accountCheckboxes).filter(cb => cb !== clickedCheckbox);
-
             otherCheckboxes.forEach(cb => cb.checked = false);
+
+            for(let i = 0; i < accountCheckboxes.length; i++) {
+                if (accountCheckboxes[i].checked === true) {
+                    accountSelectButton.disabled = false;
+                    break;
+                } else {
+                    accountSelectButton.disabled = true;
+                }
+            }
         });
     });
 })
--- wp-content/plugins/HyperSiteReviews/includes/admin/templates/setup-page.php
+++ wp-content/plugins/HyperSiteReviews/includes/admin/templates/setup-page.php
@@ -3,31 +3,35 @@
 <?php endif; ?>
 
 <?php if ($token): ?>
-    <p><strong>Google account is connected.</strong></p>
-    <form method="post">
-        <?php wp_nonce_field('hsrev_google_disconnect'); ?>
-            <div id="account-selection-table" data-select="single">
-                <p>Select the Account you would like to use.</p>
-                <div class="account-selection-table__heading-row">
-                    <div class="account-selection-table__heading" ><p>Select</p></div>
-                    <div class="account-selection-table__heading"><p>Account ID</p></div>
-                    <div class="account-selection-table__heading"><p>Account Name</p></div>
-                    <div class="account-selection-table__heading"><p>Account Type</p></div>
-                    <div class="account-selection-table__heading"><p># of Locations</p></div>
-                </div>
-
-                <?php foreach(HyperSiteReviews::get_accounts() as $acc) : ?>
-                    <div class="account-row-item" data-account-id=<?php echo HyperSiteReviews::get_google_account_id($acc); ?>>
-                        <input type="checkbox" name="selected-account">
-                        <div class="account-row-item__cell" data-type="name"><?php echo $acc['name']; ?></div>
-                        <div class="account-row-item__cell" data-type="account-name"><?php echo $acc['accountName']; ?></div>
-                        <div class="account-row-item__cell" data-type="type"><?php echo $acc['type']; ?></div>
-                        <div class="account-row-item__cell" data-type="location-count"><?php echo HyperSiteReviews::get_account_locations_length($acc['name'], false); ?></div>
+    <div class="setup-page" data-page="1">
+        <form method="post">
+            <?php wp_nonce_field('hsrev_google_disconnect'); ?>
+                <div id="account-selection-table" data-select="single">
+                    <p>Select the Account you would like to use.</p>
+                    <div class="account-selection-table__heading-row">
+                        <div class="account-selection-table__heading" ><p>Select</p></div>
+                        <div class="account-selection-table__heading"><p>Account ID</p></div>
+                        <div class="account-selection-table__heading"><p>Account Name</p></div>
+                        <div class="account-selection-table__heading"><p>Account Type</p></div>
+                        <div class="account-selection-table__heading"><p># of Locations</p></div>
                     </div>
-                <?php endforeach ?>
-            </div>
-        <input type="submit" name="disconnect" value="Disconnect Google Account" class="button button-secondary">
-    </form>
+
+                    <?php foreach(HyperSiteReviews::get_accounts() as $acc) : ?>
+                        <div class="rows">
+                            <div class="account-row-item" data-account-id=<?php echo HyperSiteReviews::get_google_account_id($acc); ?>>
+                                <input type="checkbox" name="selected-account">
+                                <div class="account-row-item__cell" data-type="name"><?php echo $acc['name']; ?></div>
+                                <div class="account-row-item__cell" data-type="account-name"><?php echo $acc['accountName']; ?></div>
+                                <div class="account-row-item__cell" data-type="type"><?php echo $acc['type']; ?></div>
+                                <div class="account-row-item__cell" data-type="location-count"><?php echo HyperSiteReviews::get_account_locations_length($acc['name'], false); ?></div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                    <button class="page-next" data-page="1" disabled></button>
+                </div>
+            <input type="submit" name="disconnect" value="Disconnect Google Account" class="button button-secondary">
+        </form>
+    </div>
     <p><a href="<?php echo esc_url(admin_url('admin.php?page=hypersite-reviews')); ?>" class="button button-primary">Continue to Plugin</a></p>
 <?php else: ?>
     <p><a href="<?php echo esc_url($authUrl); ?>" class="button button-primary">Connect with Google</a></p>
