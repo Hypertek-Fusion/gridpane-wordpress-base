5003c4e66be026a21a30c5c0dae7c2aa155c8a46
Hunter <hunter@MacBook-Pro-2.local>
2025-06-04 12:22:56 -0400

    Removed title from readMask when pulling locations

--- wp-content/plugins/HyperSiteReviews/includes/class-hypersite-reviews.php
+++ wp-content/plugins/HyperSiteReviews/includes/class-hypersite-reviews.php
@@ -312,7 +312,7 @@ class HyperSiteReviews {
                 $curr_account = $account->getName();
                 $response = $service->accounts_locations->listAccountsLocations(
                     $curr_account,
-                    ['readMask' => 'name,title,locationName']
+                    ['readMask' => 'name,locationName']
                 );
                 foreach($response as $location) {
                     self::$account_locations[$curr_account] = $location;
