24d0f91f3a189da1e3aede86fb89792af80f129f
Hunter <hunter@Hunters-Macbook.local>
2025-06-05 12:55:36 -0400

    Added debug logging

--- wp-content/plugins/HyperSiteReviews/includes/class-hypersite-reviews.php
+++ wp-content/plugins/HyperSiteReviews/includes/class-hypersite-reviews.php
@@ -18,17 +18,22 @@ class HyperSiteReviews {
             add_action('admin_init', [self::class, 'maybe_redirect_to_setup']);
 
             add_action('rest_api_init', function () {
-                // Authenticate user via cookie
                 if (!is_user_logged_in() && isset($_COOKIE[LOGGED_IN_COOKIE])) {
+                    error_log('LOGGED_IN_COOKIE value: ' . $_COOKIE[LOGGED_IN_COOKIE]);
                     $user_id = wp_validate_auth_cookie($_COOKIE[LOGGED_IN_COOKIE], 'logged_in');
                     if ($user_id) {
+                        error_log('Authenticated user ID: ' . $user_id);
                         wp_set_current_user($user_id);
+                    } else {
+                        error_log('Failed to authenticate user via cookie.');
                     }
+                } else {
+                    error_log('User already logged in or no LOGGED_IN_COOKIE.');
                 }
 
-                // Now register routes with the correct user context
                 HyperSiteReviews::register_api_routes();
             });
+
 
             if(HSREV_DEBUG) {
                 add_action('admin_menu', [self::class, 'add_debug_admin_menus']);
