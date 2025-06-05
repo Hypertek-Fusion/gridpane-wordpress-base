eab26122f0cb143df53730a026ac389e810258f8
Hunter <hunter@Hunters-Macbook.local>
2025-06-05 16:57:47 -0400

    Fixed getLocations function

--- wp-content/plugins/HyperSiteReviews/admin/js/admin-scripts.js
+++ wp-content/plugins/HyperSiteReviews/admin/js/admin-scripts.js
@@ -18,7 +18,6 @@ const getUsers = async () => {
         accountRowsContainer.innerHTML = '';
         const loading = document.createElement('div');
         loading.style.textAlign = 'center';
-        loading.style.verticalAlign = 'center';
         loading.innerText = 'Fetching accounts. Please wait ...';
         loading.classList.add('account-row-item');
         accountRowsContainer.appendChild(loading);
@@ -97,19 +96,10 @@ const getAccountLocationsLength = async (accountName) => {
     }
 };
 
-// Function to populate locations for a selected account
-const populateLocations = async (accountId) => {
+// Function to fetch locations for a selected account
+const getLocations = async (accountId) => {
     try {
-        const locationRowsContainer = document.getElementById('location-rows');
-        locationRowsContainer.innerHTML = '';
-        const loading = document.createElement('div');
-        loading.style.textAlign = 'center';
-        loading.style.verticalAlign = 'center';
-        loading.innerText = 'Fetching locations. Please wait ...';
-        loading.classList.add('location-row-item');
-        locationRowsContainer.appendChild(loading);
-
-        const url = getAccountLocationsUrl(accountId);
+        const url = getAccountLocationsUrl(accountId.replace('%s', accountName.replace('accounts/', '')));
         const response = await fetch(url, {
             method: 'GET',
             headers: {
@@ -124,33 +114,44 @@ const populateLocations = async (accountId) => {
 
         const locationsData = await response.json();
         console.log('Locations for Account:', locationsData);
-
-        locationRowsContainer.innerHTML = ''; // Clear loading message
-
-        locationsData.forEach(location => {
-            const locationRow = document.createElement('div');
-            locationRow.classList.add('rows');
-            locationRow.innerHTML = `
-                <div class="location-row-item" data-location-id="${location.name}">
-                    <input type="checkbox" name="selected-location">
-                    <div class="location-row-item__cell" data-type="id">${location.name}</div>
-                    <div class="location-row-item__cell" data-type="name">${location.title}</div>
-                    <div class="location-row-item__cell" data-type="review-count">Loading...</div>
-                </div>
-            `;
-            locationRowsContainer.appendChild(locationRow);
-        });
-
-        if (window.HSRevData.functions.attachCheckboxListeners) {
-            window.HSRevData.functions.attachCheckboxListeners(locationRowsContainer);
-        }
-
+        populateLocations(locationsData); // Call populateLocations with the data
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
+    }
+};
+
+// Function to populate locations for a selected account
+const populateLocations = (locationsData) => {
+    const locationRowsContainer = document.getElementById('location-rows');
+    locationRowsContainer.innerHTML = '';
+    const loading = document.createElement('div');
+    loading.style.textAlign = 'center';
+    loading.innerText = 'Fetching locations. Please wait ...';
+    loading.classList.add('location-row-item');
+    locationRowsContainer.appendChild(loading);
+
+    locationRowsContainer.innerHTML = ''; // Clear loading message
+
+    locationsData.forEach(location => {
+        const locationRow = document.createElement('div');
+        locationRow.classList.add('rows');
+        locationRow.innerHTML = `
+            <div class="location-row-item" data-location-id="${location.name}">
+                <input type="checkbox" name="selected-location">
+                <div class="location-row-item__cell" data-type="id">${location.name}</div>
+                <div class="location-row-item__cell" data-type="name">${location.title}</div>
+                <div class="location-row-item__cell" data-type="review-count">Loading...</div>
+            </div>
+        `;
+        locationRowsContainer.appendChild(locationRow);
+    });
+
+    if (window.HSRevData.functions.attachCheckboxListeners) {
+        window.HSRevData.functions.attachCheckboxListeners(locationRowsContainer);
     }
 };
 
 // Expose functions to the global scope
 window.HSRevData = window.HSRevData || {};
 window.HSRevData.functions = window.HSRevData.functions || {};
-window.HSRevData.functions.populateLocations = populateLocations;
+window.HSRevData.functions.getLocations = getLocations;
