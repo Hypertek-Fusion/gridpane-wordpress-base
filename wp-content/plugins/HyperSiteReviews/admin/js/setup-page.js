fc6b662f6fc02d08955daa34fcf5e3a562e6d61b
Hunter <hunter@Hunters-Macbook.local>
2025-06-05 16:28:53 -0400

    Updated order of isAnyAccountChecked

--- wp-content/plugins/HyperSiteReviews/admin/js/setup-page.js
+++ wp-content/plugins/HyperSiteReviews/admin/js/setup-page.js
@@ -4,6 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const nextButton = document.querySelector('.page-next');
     let currentPage = 0;
 
+    // Account selection logic
+    const accountSelect = document.getElementById('account-selection-table');
+
+    // Function to check if any account is selected
+    const isAnyAccountChecked = () => {
+        const accountCheckboxes = accountSelect.querySelectorAll('input[type="checkbox"]');
+        return Array.from(accountCheckboxes).some(cb => cb.checked);
+    };
+
+    const updateButtonState = () => {
+        nextButton.disabled = !isAnyAccountChecked();
+    };
+
+    // Function to attach event listeners to checkboxes
+    const attachCheckboxListeners = () => {
+        const accountCheckboxes = accountSelect.querySelectorAll('input[type="checkbox"]');
+        accountCheckboxes.forEach(c => {
+            c.addEventListener('change', e => {
+                const clickedCheckbox = e.target;
+                const otherCheckboxes = Array.from(accountCheckboxes).filter(cb => cb !== clickedCheckbox);
+                otherCheckboxes.forEach(cb => cb.checked = false);
+
+                updateButtonState();
+            });
+        });
+    };
+
     // Function to show the current page and hide others
     const showPage = (index) => {
         pages.forEach((page, i) => {
@@ -32,33 +59,6 @@ document.addEventListener('DOMContentLoaded', () => {
             showPage(currentPage);
         }
     });
-
-    // Account selection logic
-    const accountSelect = document.getElementById('account-selection-table');
-
-    // Function to check if any account is selected
-    const isAnyAccountChecked = () => {
-        const accountCheckboxes = accountSelect.querySelectorAll('input[type="checkbox"]');
-        return Array.from(accountCheckboxes).some(cb => cb.checked);
-    };
-
-    const updateButtonState = () => {
-        nextButton.disabled = !isAnyAccountChecked();
-    };
-
-    // Function to attach event listeners to checkboxes
-    const attachCheckboxListeners = () => {
-        const accountCheckboxes = accountSelect.querySelectorAll('input[type="checkbox"]');
-        accountCheckboxes.forEach(c => {
-            c.addEventListener('change', e => {
-                const clickedCheckbox = e.target;
-                const otherCheckboxes = Array.from(accountCheckboxes).filter(cb => cb !== clickedCheckbox);
-                otherCheckboxes.forEach(cb => cb.checked = false);
-
-                updateButtonState();
-            });
-        });
-    };
 
     // Call this after populating account checkboxes dynamically
     attachCheckboxListeners();
