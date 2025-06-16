fdf44ad37eeed85de14627289f677c57ee138609
Hunter <hunter@Hunters-Macbook.local>
2025-06-16 16:05:23 -0400

    Added getSelectedLocation function

--- wp-content/plugins/HyperSiteReviews/admin/js/main-page.js
+++ wp-content/plugins/HyperSiteReviews/admin/js/main-page.js
@@ -1,3 +1,26 @@
+const getSelectedLocation = async () => {
+    try {
+        const response = await fetch(window.HSRevApi.urls.selectedLocation, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-WP-Nonce': HSRevApi.nonce
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const locationsData = await response.json();
+
+        return locationsData;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        locationRowsContainer.innerHTML = '<div style="text-align: center;" class="row-item">Error fetching locations. Please try again later.</div>';
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     if (window.HyperSiteReviews) {
         await window.HSRevData.functions.prefetchReviews();
