2f288182bfe201febaf2b0930e248822fcff0ace
Hunter <hunter@Hunters-Macbook.local>
2025-06-17 12:43:22 -0400

    Added console log

--- wp-content/plugins/HyperSiteReviews/admin/js/main-page.js
+++ wp-content/plugins/HyperSiteReviews/admin/js/main-page.js
@@ -91,6 +91,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 // For every nth review, where n = reviews per page (rpp), append a row
                 for(let j = 0; j < rpp; j++) {
                     const review = reviews[(i * 10) + j];
+                    console.log('Review #: ', (i * 10) + j)
                     // Create new row item
                     row.innerHTML += `
                         <div class="row-item">
