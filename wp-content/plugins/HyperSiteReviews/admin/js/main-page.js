30387acfdcfe8931d2ca3a2aff0b4d7da8100349
Hunter <hunter@Hunters-Macbook.local>
2025-06-17 15:30:34 -0400

    Another test

--- wp-content/plugins/HyperSiteReviews/admin/js/main-page.js
+++ wp-content/plugins/HyperSiteReviews/admin/js/main-page.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', async () => {
-
     const getSelectedLocation = () => {
     return new Promise(async (resolve, reject) => {
         const response = await fetch(window.HSRevApi.urls.selectedLocation, {
