9a620a32eba946b6a3e46a70eb6fae1d3f166950
Hunter <hunter@Hunters-Macbook.local>
2025-06-17 15:10:48 -0400

    Updated build config for Vite

--- wp-content/plugins/HyperSiteReviews/vite.config.js
+++ wp-content/plugins/HyperSiteReviews/vite.config.js
@@ -1,20 +1,21 @@
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-
-export default {
+export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        forms: resolve(__dirname, '/admin/js/forms.js'),
-        admin: resolve(__dirname, '/admin/js/admin-scripts.js'),
-        mainPage: resolve(__dirname, '/admin/js/main-page.js'),
-        setupPage: resolve(__dirname, '/admin/js/setup-page.js'),
+        forms: resolve(__dirname, 'admin/js/forms.js'),
+        admin: resolve(__dirname, 'admin/js/admin-scripts.js'),
+        mainPage: resolve(__dirname, 'admin/js/main-page.js'),
+        setupPage: resolve(__dirname, 'admin/js/setup-page.js'),
       },
       output: {
         entryFileNames: '[name].js',
-      },
+      }
     },
-  },
-};
+    minify: 'esbuild' 
+  }
+});
