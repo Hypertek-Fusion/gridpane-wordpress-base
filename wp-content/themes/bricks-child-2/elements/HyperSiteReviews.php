e46f61f9fd0dfe02f6b2d823645ed02540b088f7
Hunter <hunter@Hunters-Macbook.local>
2025-06-19 13:41:27 -0400

    Added function for outputting cards

--- wp-content/themes/bricks-child-2/elements/HyperSiteReviews.php
+++ wp-content/themes/bricks-child-2/elements/HyperSiteReviews.php
@@ -8,7 +8,7 @@ class Prefix_Element_Test extends \Bricks\Element {
   public $name         = 'HyperSite Reviews';
   public $icon         = 'fas fa-comment';
   public $css_selector = 'hypersite-reviews';
-  public $scripts      = ['hsrev-frontend-widget'];
+  public $scripts      = ['hsrev-frontend-widget, bricksSplide'];
   public $nestable     = false; // true || @since 1.5
 
   // Methods: Builder-specific
@@ -17,8 +17,7 @@ class Prefix_Element_Test extends \Bricks\Element {
     return esc_html__( 'HyperSite Reviews', 'bricks');
   }
   public function get_keywords() {}
-  public function set_control_groups() {}
-  public function set_controls() {
+  public function set_control_groups() {
     $this->control_groups['text'] = [ // Unique group identifier (lowercase, no spaces)
       'title' => esc_html__( 'Text', 'bricks' ), // Localized control group title
       'tab' => 'content', // Set to either "content" or "style"
@@ -28,13 +27,38 @@ class Prefix_Element_Test extends \Bricks\Element {
       'title' => esc_html__( 'Settings', 'bricks' ),
       'tab' => 'content',
     ];
+  }
+  public function set_controls() {
+    $this->controls['selectDisplayType'] = [
+      'tab' => 'content',
+      'label' => esc_html__( 'Display Type', 'bricks' ),
+      'type' => 'select',
+      'options' => [
+        'grid' => 'Grid',
+        'slider' => 'Slider'
+      ],
+      'inline' => true,
+      'placeholder' => esc_html__( 'Select tag', 'bricks' ),
+      'multiple' => false, 
+      'searchable' => false,
+      'clearable' => false,
+      'default' => 'grid',
+    ];
 
   }
 
   // Methods: Frontend-specific
   public function enqueue_scripts() {
-    if (!wp_script_is('hypersite-reviews-stylesheet', 'enqueued')) {
+    if (!wp_style_is('hypersite-reviews-stylesheet', 'enqueued')) {
         wp_enqueue_style('hypersite-reviews-stylesheet', HSREV_URL . 'public/css/hsrev-main.css', [], '1.0', false);
+    }
+
+    if(!wp_script_is('bricks-splide', 'enqueued')) {
+      wp_enqueue_script( 'bricks-splide' );
+    }
+
+    if(!wp_style_is('bricks-splide', 'enqueued')) {
+      wp_enqueue_style( 'bricks-splide' );
     }
   }
   public function render() {
@@ -49,58 +73,57 @@ class Prefix_Element_Test extends \Bricks\Element {
     $total_overall_reviews = GoogleDataHandler::get_location_reviews_length($location_id);
 
 
-    // Prefetched Assets
-    $google_logo = file_get_contents(HSREV_URL . 'public/images/google-logo-borderless.svg');
-    $star_svg = file_get_contents(HSREV_URL . 'public/images/star-fill.svg');
-
-    error_log('Per Page: '. $per_page);
-    error_log('Location Id: '. $location_id);
-    error_log('Total reviews: '. $total_selected_reviews);
-    error_log('Pages: ' . $pages);
-
-    $this->set_attribute('_root', 'class', $root_classes);
-
-    echo "<div {$this->render_attributes( '_root' )}>";
-
     for($i = 0; $i < $pages; $i++) {
         $reviews = GoogleDataHandler::get_selected_reviews($location_id, $i + 1, $per_page);
         
         $reviews_batch = array_merge($reviews_batch, $reviews);
     }
 
-    echo "Total Reviews: " . $total_overall_reviews;
+    // Prefetched Assets
+    $google_logo = file_get_contents(HSREV_URL . 'public/images/google-logo-borderless.svg');
+    $star_svg = file_get_contents(HSREV_URL . 'public/images/star-fill.svg');
+
+    $output_card = function($review) use ($google_logo, $star_svg) {
+      ob_start();
+      ?>
+      <div class="testimonial-card">
+        <div class="testimonial-card__top-wrapper">
+          <div class="testimonial-card__author-profile-icon-wrapper">
+            <img class="testimonial-card__author-profile-icon" src="<?php echo $review['reviewer_profile_photo_url']; ?>">
+          </div>
+          <div class="testimonial-card__author-name-wrapper">
+            <p class="testimonial-card__author-name"><?php echo $review["reviewer_display_name"]; ?></p>
+            <div class="star-ratings-wrapper">
+              <?php
+                for($i = 0; $i < 5; $i++) {
+                  echo $star_svg;
+                }
+              ?>
+            </div>
+          </div>
+        </div>
+        <div class="testimonial-card__content-wrapper">
+          <p class="testimonial-card__content"><?php echo $review["comment"]; ?></p>
+        </div>
+        <div class="testimonial-card__bottom-wrapper">
+          <p><?php echo date('Y-m-d', strtotime($review["create_time"])); ?></p>
+          <?php echo $google_logo; ?>
+        </div>
+      </div>
+      <?php
+      return ob_get_clean();
+    };
+
+    $this->set_attribute('_root', 'class', $root_classes);
+
+    echo "<div {$this->render_attributes( '_root' )}>";
 
     ?>
   <div class="testimonials-wrapper">
   <?php
 
     foreach($reviews_batch as $review) {
-?>
-    <div class="testimonial-card">
-      <div class="testimonial-card__top-wrapper">
-        <div class="testimonial-card__author-profile-icon-wrapper">
-          <img class="testimonial-card__author-profile-icon" src="<?php echo $review['reviewer_profile_photo_url']; ?>">
-        </div>
-        <div class="testimonial-card__author-name-wrapper">
-          <p class="testimonial-card__author-name"><?php echo $review["reviewer_display_name"]; ?></p>
-          <div class="star-ratings-wrapper">
-            <?php
-              for($i = 0; $i < 5; $i++) {
-                echo $star_svg;
-              }
-            ?>
-          </div>
-        </div>
-      </div>
-      <div class="testimonial-card__content-wrapper">
-        <p class="testimonial-card__content"><?php echo $review["comment"]; ?></p>
-      </div>
-      <div class="testimonial-card__bottom-wrapper">
-        <p><?php echo date('Y-m-d', strtotime($review["create_time"])); ?></p>
-        <?php echo $google_logo; ?>
-      </div>
-    </div>
-<?php
+      echo $output_card($review);
     }
 
 ?>
